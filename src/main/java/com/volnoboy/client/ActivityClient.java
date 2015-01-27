c5d988ebc081bef44bb61217a64d56e211899491
volnoboy <volnoboy@gmail.com>
2015-01-27 14:15:01 +0100

    Create client and tests

---
+++ src/main/java/com/volnoboy/client/ActivityClient.java
@@ -0,0 +1,8 @@
+package com.volnoboy.client;
+
+/**
+ * @author Volodymyr Volnoboy (vvolnoboy@luxoft.com)
+ * @since 1/27/15 11:55 AM
+ */
+public class ActivityClient {
+}
---
+++ src/test/java/com.volnoboy.client/ActivityClientTest.java
@@ -0,0 +1,8 @@
+package com.volnoboy.client;
+
+/**
+ * @author Volodymyr Volnoboy (vvolnoboy@luxoft.com)
+ * @since 1/27/15 12:06 PM
+ */
+public class ActivityClientTest {
+}
